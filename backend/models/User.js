9682cbc632b8918439b3f797ce720a0c29196957
Mansi Vaghasiya <vaghasiyamansi999@gmail.com>
2026-01-31 18:51:45 +0530

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: { type: String, unique: true },
+    password: String,
+    role: {
+      type: String,
+      enum: ["admin", "faculty", "student"],
+      default: "student",
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "active",
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+export default mongoose.model("User", userSchema);
